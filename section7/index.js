b72ce1ff5e691f774d3470a9a21fa73dc1a926c8
joelsoncrj <joelsonrocha.eng@gmail.com>
2023-01-07 00:01:10 -0300

    up

---
+++ section7/index.js
@@ -0,0 +1,59 @@
+const { read } = require('fs');
+const http = require('http');
+const PORT = 3000;
+
+const friends =[
+    {
+        name:'Sir Isaac Newton',
+         id: 0,
+    },
+    {
+        name:'joelson',
+         id: 1,
+    },
+    {
+        name:'eloisa',
+         id: 2,
+    },
+    {
+        name:'luiz',
+         id: 3,
+    },
+    {
+        name:'marcos',
+         id: 4,
+    }
+]
+const server = http.createServer((req,res)=>{
+    const items = req.url.split('/');
+    if(items[1]==='friends'){
+
+        res.statusCode  = 200;
+        res.setHeader('Content-Type','application/json');
+        if(items.length ===3){
+            const friendIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]))
+        }else{
+            res.end(JSON.stringify(friends));
+        }
+
+    }else if (items[1] === 'messages'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>')
+        res.write('<li>Hello Isaac !</li>');
+        res.write('<li>What are your thoughts  on astronomy</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }  else{
+        res.statusCode=404;
+        res.end();
+    }
+});
+
+server.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT} ...`)
+});//127.0.0.1 or localhost
